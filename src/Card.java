a8536a4e063b664d3e368b4a7748fe4cd8ee970d
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-02-08 12:38:41 -0500

    first commit

---
+++ src/Card.java
@@ -0,0 +1,16 @@
+
+public class Card {
+int Number;
+String suit;
+
+	public Card(int Num,String S) {
+		// TODO Auto-generated constructor stub
+
+	Number=Num;
+	suit=S;
+	
+	
+	}
+
+	
+}
---
+++ src/CardTest.java
@@ -0,0 +1,29 @@
+
+public class CardTest {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Card card1type1=new Card(1,"Clubs");	
+	Card card2type1= new Card(3,"Spades");	
+	Card card3type1=new Card(5,"Hearts");
+	
+	Card card4type2=new Card(2,"Diamonds");	
+	Card card5type2= new Card(4,"Hearts");	
+	Card card6type2=new Card(6,"Spades");
+		
+	Card card7type3=new Card(7,"Spades");	
+	Card card8type3= new Card(8,"Clubs");	
+	Card Lucky=new Card(9,"Diamonds");	
+		
+	System.out.println(card2type1.Number + card3type1.suit);	
+		
+		
+		
+		
+	}
+
+	
+	
+}
